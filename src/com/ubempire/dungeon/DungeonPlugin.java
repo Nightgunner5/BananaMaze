605a84da94fe6c7eea7af6a1effee7e3eb474f52
Ben L <nightgunner5@llamaslayers.net>
2011-07-13 15:23:53 -0500

    0.1.1 Bugfix from SpaceManiac

--- src/com/ubempire/dungeon/DungeonPlugin.java
+++ src/com/ubempire/dungeon/DungeonPlugin.java
@@ -6,6 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class DungeonPlugin extends JavaPlugin {
 	private static final DungeonGenerator dg = new DungeonGenerator();
+	private boolean inBukkitYML = false;
 
 	@Override
 	public void onDisable() {
@@ -14,11 +15,20 @@ public class DungeonPlugin extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		getServer().createWorld("dungeon", Environment.NORMAL, dg);
+		getServer().getScheduler().scheduleSyncDelayedTask(this,
+				new Runnable() {
+					public void run() {
+						if (!inBukkitYML) {
+							getServer().createWorld("dungeon",
+									Environment.NORMAL, dg);
+						}
+					}
+				});
 	}
 
 	@Override
 	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+		inBukkitYML = true;
 		return dg;
 	}
 }
