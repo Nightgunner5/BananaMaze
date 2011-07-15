3c72cfe479e7baebf9d0ef5b02a73ce05ef8cb74
Ben L <nightgunner5@llamaslayers.net>
2011-07-15 08:54:03 -0500

    Fix chest populator, add .gitignore

--- src/net/llamaslayers/minecraft/banana/populators/ChestPopulator.java
+++ src/net/llamaslayers/minecraft/banana/populators/ChestPopulator.java
@@ -3,6 +3,7 @@ package net.llamaslayers.minecraft.banana.populators;
 import java.util.Random;
 
 import org.bukkit.Chunk;
+import org.bukkit.ChunkSnapshot;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
@@ -39,9 +40,12 @@ public class ChestPopulator extends BlockPopulator {
 	@Override
 	public void populate(World world, Random random, Chunk source) {
 		if (random.nextInt(100) < CHANCE_OF_CHEST) {
-			Block block = world.getHighestBlockAt(
-					source.getX() << 4 + 1 + random.nextInt(14),
-					source.getZ() << 4 + 1 + random.nextInt(14));
+			ChunkSnapshot snapshot = source.getChunkSnapshot();
+			int x = 1 + random.nextInt(14);
+			int z = 1 + random.nextInt(14);
+			int y = snapshot.getHighestBlockYAt(x, z);
+
+			Block block = source.getBlock(x, y, z);
 			if (block.getRelative(BlockFace.DOWN).getType() == FLOOR_MATERIAL) {
 				block.setType(CHEST_MATERIAL);
 				Chest chest = (Chest) block.getState();
